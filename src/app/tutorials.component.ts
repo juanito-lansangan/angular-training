828d354bb8ef0a80e00b80d13840f3b19062fcb6
jplans <juanito.lansangan@gmail.com>
2017-06-01 11:04:42 +0800

    angular interpolation and data binding

--- src/app/tutorials.component.ts
+++ src/app/tutorials.component.ts
@@ -2,8 +2,10 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'my-tutorials',
-    template: '<h2>Awesome Tutorials</h2>'
+    template: `<h2>{{ title }}</h2>
+               <img [src]="imageUrl" />`
 })
 export class TutorialsComponent {
-
+    public title = "Awesome Angular Tutorial";
+    public imageUrl = "http://lorempixel.com/400/200";
 }
