cd2de44db78dbdd8b16ef179ed11c648effa5490
DESKTOP-PAUACCH\All <firefoxm32@gmail.com>
2017-06-01 10:29:57 +0800

    initial commit

---
+++ src/app/tutorials.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'my-tutorials',
+    template: '<h2>JPlans awesome Tutorials</h2>'
+})
+export class TutorialsComponent {
+
+}
