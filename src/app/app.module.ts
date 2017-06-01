be3f480a510c347b27f890e40734c3e25d12ac0d
jplans <juanito.lansangan@gmail.com>
2017-06-01 13:47:18 +0800

    Two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TutorialsComponent } from './tutorials.component';
 import { ClassStyleBindingComponent } from './tutorials/class-style-binding.component';
 import { EventBindingReferenceComponent } from "./tutorials/event-binding-reference.component";
+import { TwoWayDataBinding } from "./tutorials/two-way-data-binding.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     TutorialsComponent,
     ClassStyleBindingComponent,
-    EventBindingReferenceComponent
+    EventBindingReferenceComponent,
+    TwoWayDataBinding
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tutorials/two-way-data-binding.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'two-way-binding',
+    template: `<h1>{{title}}</h1>
+               <input type="text" [(ngModel)]="_fname" />
+               <input type="text" [(ngModel)]="_lname" />
+               <h3>{{_fname}} {{_lname}}</h3>`
+})
+export class TwoWayDataBinding  {
+    title = "Two Way Binding";
+    // fname;
+    // lname;
+}
